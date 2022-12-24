9eed20150671fc7f99e01651ab45a166ce6c9092
marcosChalet <chaletmarcos@gmail.com>
2022-12-24 14:28:46 -0300

    :sparkles: feat: Adicionando a funcionalidade de remover

--- js/script.js
+++ js/script.js
@@ -3,14 +3,12 @@ const assunto = document.getElementById('assunto')
 
 const itens = document.getElementById('todo-list')
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault()
+let totalItens = 0
 
+function createTodoItem() {
   const tituloNovo = document.createElement('h1')
   const assuntoNovo = document.createElement('p')
-  const itemNovo = document.createElement('div')
   const descricao = document.createElement('div')
-  const icones = document.createElement('div')
 
   const textTitulo = document.createTextNode(titulo.value)
   const textAssunto = document.createTextNode(assunto.value)
@@ -18,24 +16,48 @@ form.addEventListener('submit', (e) => {
   tituloNovo.append(textTitulo)
   assuntoNovo.append(textAssunto)
 
+  descricao.append(tituloNovo)
+  descricao.append(assuntoNovo)
+
+  return descricao
+}
+
+function createIconsItem() {
+  const icones = document.createElement('div')
   const hamburguer = document.createElement('i')
+  const lixeira = document.createElement('i')
+
   hamburguer.classList.add('fa-solid')
   hamburguer.classList.add('fa-bars')
   hamburguer.classList.add('icon')
-  const lixeira = document.createElement('i')
+
   lixeira.classList.add('fa-solid')
   lixeira.classList.add('fa-trash-can-arrow-up')
   lixeira.classList.add('icon')
+  lixeira.setAttribute('id', `${++totalItens}`)
+  lixeira.addEventListener('click', event => {
+    event.stopPropagation()
+    const id = event.target.id
+    const nodeRemove = document.getElementById(`child-${id}`)
+    nodeRemove.parentNode.removeChild(nodeRemove)
+  })
 
   icones.classList.add('icons-item')
   icones.append(lixeira)
   icones.append(hamburguer)
-  descricao.append(tituloNovo)
-  descricao.append(assuntoNovo)
 
-  itemNovo.append(descricao)
-  itemNovo.append(icones)
+  return icones
+}
 
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  const itemNovo = document.createElement('div')
+
+  itemNovo.append(createTodoItem())
+  itemNovo.append(createIconsItem())
+
+  itemNovo.setAttribute('id', `child-${totalItens}`)
   itemNovo.classList.add('item')
   itens.insertBefore(itemNovo, itens.children[0])
 })
